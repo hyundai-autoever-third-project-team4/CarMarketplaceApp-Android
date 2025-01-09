9beb2fb1abd4d7211412ba8a05c0c4af0e9d097f
yeon-dong <jae073@naver.com>
2025-01-09 12:31:29 +0900

    컴파일 api 수준 35로 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.autoever.chajava"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.autoever.chajava"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
